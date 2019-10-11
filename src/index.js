c7faabf8277f6bf0c7635e988ed6a1db70f2e2a8
Pavel Karenda <korendos@gmail.com>
2019-10-11 14:21:55 +0300

    feat: solution for task js_edu

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@
  * @returns number of weeks needed for finish education
  */
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
+    focus,
+    knowsProgramming,
+    config) {
+    if (knowsProgramming) {
+        return  Math.ceil(800 / config[focus]);
+    }
+      return Math.ceil((800 + 500) / config[focus]);
   };
-  
